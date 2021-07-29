fe1d33984eb318a2bec67c7dff23d4c67dc0df11
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-07-30 02:52:11 +0700

    - add AdminBSBMaterialDesign Theme

--- application/views/admin/auth/login.php
+++ application/views/admin/auth/login.php
@@ -1,48 +1,93 @@
-<?php $this->load->view('header'); ?>
+﻿<!DOCTYPE html>
+<html>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-6 offset-lg-3">
-                <div class="card">
-                    <div class="card-header">
-                        Admin Login
-                    </div>
-                    <div class="card-body">
-                        <form action="<?php echo base_url('admin/login');?>" method="post">
-                            <div class="form-group">
-                                <label>Username</label>
-                                <input type="text" name="username" placeholder="Username" class="form-control" value="<?php echo set_value('username'); ?>"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Password</label>
-                                <input type="password" name="password" placeholder="Password" class="form-control"/>
-                            </div>
-                            <div class="form-group">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" value="1" <?php echo set_checkbox('remember',1);?>>
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Sign In | Bootstrap Based Admin Template - Material Design</title>
+    <!-- Favicon-->
+    <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
 
-                                    <label class="form-check-label" for="remember">
-                                        Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="/assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="/assets/plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="/assets/plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+            <a href="javascript:void(0);">Admin<b>BSB</b></a>
+            <small>Admin BootStrap Based - Material Design</small>
+        </div>
+        <div class="card">
+            <div class="body">
+                <form id="sign_in" action="<?php echo base_url('admin/login');?>" method="POST">
+                    <div class="msg">Sign in to start your session</div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">person</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <div class="d-flex justify-content-center">
-                            Don't have an account?&nbsp;<a href="<?php echo base_url('admin/register');?>"> Sign Up</a>
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <a href="<?php echo base_url('admin/forgot_password');?>">Forgot your password?</a>
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <a href="<?php echo base_url('admin/resend_activation');?>">Didn't receive the activation email?</a>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">lock</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
                         </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-xs-8 p-t-5">
+                            <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                            <label for="rememberme">Remember Me</label>
+                        </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-block bg-pink waves-effect" type="submit">SIGN IN</button>
+                        </div>
+                    </div>
+                    <div class="row m-t-15 m-b--20">
+                        <div class="col-xs-6">
+                            <a href="sign-up.html">Register Now!</a>
+                        </div>
+                        <div class="col-xs-6 align-right">
+                            <a href="forgot-password.html">Forgot Password?</a>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 
-<?php $this->load->view('footer'); ?>
+    <!-- Jquery Core Js -->
+    <script src="/assets/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="/assets/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="/assets/plugins/node-waves/waves.js"></script>
+
+    <!-- Validation Plugin Js -->
+    <script src="/assets/plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- Custom Js -->
+    <script src="/assets/js/admin.js"></script>
+    <script src="/assets/js/pages/examples/sign-in.js"></script>
+</body>
+
+</html>
